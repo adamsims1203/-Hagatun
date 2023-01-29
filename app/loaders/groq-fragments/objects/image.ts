8ec528cd7f8aa49078048acdd3ebeed220bcd042
Adam Sims <adamsims1203@gmail.com>
2023-01-28 20:53:50 -0500

    feat: add proper image handlers

--- app/loaders/groq-fragments/objects/image.ts
+++ app/loaders/groq-fragments/objects/image.ts
@@ -1,8 +1,13 @@
+import { SanityImageObject } from '@sanity/image-url/lib/types/types'
 import groq from 'groq'
 
-export type ImageSrc = {
+export type ImageSrc = SanityImageObject & {
 	src: string
-	alt: string
+  alt: string
+	id: string
+	type: 'image/svg+xml' | 'image/jpeg'
+	aspectRatio: number
+	lqip: string
 }
 
 export const image = groq`
