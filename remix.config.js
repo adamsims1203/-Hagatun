a3833bfdf14f07892e63a315026560c74bf5ff4e
Adam Sims <adamsims1203@gmail.com>
2023-02-04 13:14:43 -0500

    fix(remix.config): update watchPaths

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,6 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-	watchPaths: ['studio']
+	watchPaths: ['sanity']
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
